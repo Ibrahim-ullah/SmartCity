f52646c0332dca3075dd89434ed957461ddfc56a
Md. Mahbubur Rahman <mahbubcu14@gmail.com>
2017-10-05 02:25:39 +0600

    Smart City App final versions

---
+++ app/src/main/java/eee/cu/ac/bd/smartcity/shortfeed.java
@@ -0,0 +1,75 @@
+package eee.cu.ac.bd.smartcity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+/**
+ * Created by mahbub on 10/2/17.
+ */
+
+public class shortfeed {
+    private String Title;
+    private String Name;
+    private String Location;
+    private String id;
+    Bitmap bitImage;
+
+
+    public shortfeed(){
+
+
+    }
+
+
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+
+
+    public Bitmap getBitImage() {
+        return bitImage;
+    }
+
+    public void setBitImage(Bitmap bitImage) {
+        this.bitImage = bitImage;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+}
+
+
